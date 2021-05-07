368191f121baf5a202be5defd9ec38d2adc4a646
Keven Fletes <keven.fletes339@gmail.com>
2021-05-07 18:48:31 -0400

    Android Studio Project Initialization. showToast Function addedin UIMessages Class

---
+++ messagesutil/src/main/java/com/example/messagesutil/UIMessages.java
@@ -0,0 +1,15 @@
+package com.example.messagesutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class UIMessages {
+    /**
+     * Show short Android Toast message
+     * @param context = Activity Context to Show Toast on
+     * @param message = Text for Toast Message
+     */
+    static void showToast(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
